e27ce87e633a435c26c3eea072bd9a32d0523416
TheSimpleWriter <simplepcwriter@gmail.com>
2019-06-09 17:28:08 +0300

    update console variant

--- console_weather.py
+++ console_weather.py
@@ -6,6 +6,10 @@ loc = requests.get(f"http://ip-api.com/json/{r.json()['ip']}")
 
 city = loc.json()['city']
 
-f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid=377ef42e9dc5e87b6c24eb7dc069c30c" """ - 5 days/3 hours """
-f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=377ef42e9dc5e87b6c24eb7dc069c30c"  """ - current """
+for_five_day = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid=377ef42e9dc5e87b6c24eb7dc069c30c" # """ - 5 days/3 hours """
+now =  f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=377ef42e9dc5e87b6c24eb7dc069c30c" # """ - current """
 
+cur = requests.get(now).json()
+
+print(f"In {city} now is {cur['weather'][0]['description']} "
+	f"(temperature {cur['main']['temp']-273.15})")
